e33abe3846320e45fa549679a703ac8424f54aa4
dominatorXX <buirakof@yandex.ru>
2023-03-25 20:10:23 +0600

    day three end

---
+++ Seminars/seminar_3/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 19
+/*
+bool isPalindrome(int number)
+{
+    int revers = 0;
+    int sourceNumber = number;
+
+    while (sourceNumber != 0)
+    {
+        int pop = sourceNumber % 10;
+        sourceNumber = sourceNumber / 10;
+
+        revers = (revers * 10) + pop;
+    }
+    bool isResult = revers == number;
+    return isResult;
+}
+bool result = isPalindrome(12821);
+System.Console.WriteLine(result);
+*/
+
+// Задача 21
+
+// Формула вычисления расстояния между двумя точками A(xa, ya) и B(xb, yb) на плоскости: AB = √((xb - xa)2 + (yb - ya)2).
+
+/*
+System.Console.WriteLine("Ввести xa");
+double xa = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Ввести ya");
+double ya = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Ввести xb");
+double xb = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Ввести yb");
+double yb = double.Parse(Console.ReadLine());
+
+double x = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+System.Console.WriteLine(Math.Round(x, 2, MidpointRounding.ToNegativeInfinity));
+*/
+
+// Задача 23
+/*
+double cubo(double arg)
+{
+    arg = Math.Abs(arg);
+
+    double count = 0;
+    double x = 1;
+
+    while (x <= arg)
+    {
+
+        if (x == arg)
+        {
+            count = Math.Pow(x, 3);
+
+            System.Console.Write(count);
+
+            break;
+        }
+
+        count = Math.Pow(x, 3);
+
+        System.Console.Write($"{count}, ");
+
+        x++;
+    }
+    return count;
+}
+
+System.Console.Write("Введите число N: ");
+double n = double.Parse(Console.ReadLine());
+
+cubo(n);
+*/
